97918ae88ee88eb5a6d2f865640fdcd2deed2249
ed-dinaoui <eddinaouibilal@gmail.com>
2022-09-01 03:46:35 -0400

    1 file modified, 1 added

---
+++ src/pages/about/components/skills/skill_imgs_arr.js
@@ -0,0 +1,20 @@
+import React from 'react' ;
+
+import icon_1 from './img/htmlcss.png' ;
+import icon_2 from './img/js.png' ;
+import icon_3 from './img/sass.png' ;
+import icon_4 from './img/pug.png' ;
+import icon_5 from './img/photoshop.png' ;
+import icon_6 from './img/figma.png' ;
+import icon_7 from './img/wordpress.png' ;
+
+
+export const s_i_arr = [
+  <img src={icon_1} /> ,
+  <img src={icon_2} /> ,
+  <img src={icon_3} /> ,
+  <img src={icon_4} /> ,
+  <img src={icon_5} /> ,
+  <img src={icon_6} /> ,
+  <img src={icon_7} /> ,
+]
--- src/pages/about/components/skills/skills.js
+++ src/pages/about/components/skills/skills.js
@@ -1,6 +1,7 @@
 import React , { memo , useEffect } from 'react' ;
 import { Link } from 'react-router-dom' ;
 import './skills.css' ;
+import { s_i_arr } from './skill_imgs_arr.js' ;
 
 let per = 0 ;
 let _speed = .1 ;
@@ -27,7 +28,7 @@ function Skill(props){
   return(
     <div data-inviewport='_skill' className="_skill"  style={{ transitionDuration : _speed + 's' }} >
       <div>
-        <img src={props.src} />
+        {props.src}
       </div>
       <div>
         <p>{props.p}</p>
@@ -45,14 +46,13 @@ function Skills(){
   return(
     <div className='_skills'>
       <div>
-        <Skill p='HTML_CSS' per='90' />
-        <Skill p='Javascript' per='60' />
-        <Skill p='NodeJs' per='60' />
-        <Skill p='MongoDB' per='70' />
-        <Skill p='React' per='90' />
-        <Skill p='Angular' per='40' />
-        <Skill p='Sass' per='60' />
-        <Skill p='Pug' per='60' />
+        <Skill p='HTML_CSS' per='90' src={s_i_arr[0]} />
+        <Skill p='Javascript' per='60'src={s_i_arr[1]} />
+        <Skill p='NodeJs' per='60' src={s_i_arr[2]} />
+        <Skill p='MongoDB' per='70' src={s_i_arr[3]} />
+        <Skill p='React' per='90' src={s_i_arr[4]} />
+        <Skill p='Angular' per='40' src={s_i_arr[5]} />
+        <Skill p='Sass' per='60' src={s_i_arr[6]} />
       </div>
       <div>
         

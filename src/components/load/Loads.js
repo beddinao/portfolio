a561b259743ccf6b697e61a8a4381efa39c295b6
ed-d <80011820+ed-dinaoui@users.noreply.github.com>
2022-09-02 21:11:16 -0400

    Update Loads.js

--- src/components/load/Loads.js
+++ src/components/load/Loads.js
@@ -1,33 +1,21 @@
 import React , {useState , useEffect , memo} from 'react' ;
 import './load.css' ;
-
-let count = 0 ;
-var images = new Array('../../pages/home/components/work/img/pawel-czerwinski-4qhg5ewdrau-unsplash.jpg','../../pages/home/components/canvas/img/bigwave_japan-new1.png','../../pages/home/components/canvas/img/augustine-wong-t0byurbdk_m-unsplash_2.jpg') ;
+import ico_1 from './ellipse.png' ;
+import ico_2 from './vector.png' ;
 
 function Loads(props){
   const [ is_visible , set_is_visible ] = useState(false) ;
 
-  function is_load(){
-    count++ ;
-    console.log('one image is done')
-    if(count == images.length){
-      set_is_visible(true) 
-    }
-  }
-
-  function img_load () {
-    for (let i = 0 ; i < images.length ; i++){
-      let img = new Image() ;
-      img.src = images[i] ;
-      img.addEventListener('load', is_load ,true ) ;
-    }
-  }
   useEffect(()=>{
-    img_load()
+    setTimeout(()=>{
+      set_is_vesibile(true)
+    },3000)
+    
   },[])
   return is_visible ? props.children : (
-    <div id='center' >
-      <h1>Loading Images ...</h1>
+    <div id='center' className='_load' >
+      <img src={ico_1} />
+      <img src={ico_2} />
     </div>
   )
 }

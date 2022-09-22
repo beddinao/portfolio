bb8c6f81f492dedc363d7a72fe3e397189da27fa
ed-d <80011820+ed-dinaoui@users.noreply.github.com>
2022-09-22 15:25:50 -0400

    Update Work.js

--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -3,6 +3,7 @@ import './Work.css' ;
 import Footer from '../../components/footer/footer';
 import Slides from '../../components/slides/slides';
 import { observe } from '../../lib/observe/observe' ;
+import set_scroll_snapping , { dis_fun } from '../../lib/scroll-snap' ;
 import { Link } from 'react-router-dom' ;
 import C_anvas from './components/canvas/c_anvas' ;
 import C_ontent from './components/content/content' ;
@@ -12,9 +13,12 @@ function Work(){
   $(document).ready(()=>{window.scrollTo( 0 , 0 ) }) ;
   useEffect(()=>{
     observe() ;
+    set_scroll_snapping( document.body.parentNode , document.querySelectorAll('#center') )
+    return () => {
+      dis_fun( document.body.parentNode )
+    }
   },[])
   return (
-
       <main>
         <C_anvas />
         <Slides h='2' c='var(--text)' c_1='var(--main-color)' >
@@ -23,7 +27,6 @@ function Work(){
         <C_ontent />
         <Footer />
       </main>
-
   )
 }
 

249e6925b7e91c5b277edc1683ac97927534b4f9
ed-d <80011820+ed-dinaoui@users.noreply.github.com>
2022-09-22 15:20:47 -0400

    Update Loads.js

--- src/components/load/Loads.js
+++ src/components/load/Loads.js
@@ -1,4 +1,5 @@
 import React , { useState , useEffect , memo } from 'react' ;
+import set_scroll_snapping , { dis_fun } from '../../lib/scroll-snap' ;
 import './load.css'
 import ico_1 from './ellipse.png' ;
 import ico_2 from './vector.png' ;
@@ -17,13 +18,20 @@ function Loads(props){
   const [ is_visible , set_is_visible ] = useState(false) ;
 
   useEffect(()=>{
-    
     setTimeout(()=>{
       set_is_visible(true) ;
-    },3000)
-    
+    },5000)
   },[])
-
+  
+  useEffect(() => {
+    if( is_visible ){
+      set_scroll_snapping( document.body.parentNode , document.querySelectorAll('#center') )
+    }
+    return () => {
+      dis_fun(document.body.parentNode)
+    }
+  })
+  
   return is_visible ? props.children : (
     <div className='_load' style={{ height : '100' + props.h }} >
       <Loading />
